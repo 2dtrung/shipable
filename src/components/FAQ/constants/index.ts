9aedc717d418e1d39097ac9be9b628e43ee86484
2dtrung <sductrung@gmail.com>
2024-06-08 14:03:37 +0700

    ✨ feat: update FAQ section

---
+++ src/components/FAQ/constants/index.ts
@@ -0,0 +1,27 @@
+export const FAQ_LIST = [
+  {
+    question: 'What do you need from me to get started?',
+    answer:
+      "Assuming we're a good fit to work together, we'll get payment out of the way and then hop on a kick-off call to get to know each other and suss out the details of the work. After that, we'll dive right in and start making ship happen for your business.",
+  },
+  {
+    question: 'How do you handle feedback?',
+    answer:
+      "The feedback loop is always open and the format is amenable to the way you want to work. If asychronous video walkthroughs are your jam, not a problem. Would you rather ping pong ideas and assets back and forth on Discord? That's cool too. It's your call.",
+  },
+  {
+    question: "No risk guarantee... what's the catch?",
+    answer:
+      "No catch. I know your time and resources aren't inifite and you have to be smart with your monthly spend. I also know the realized savings and future profit from a Shipable sprint will dwarf the cost of the service itself.",
+  },
+  {
+    question: 'Am I a good fit for your service?',
+    answer:
+      'Shipable services are primarily for tech founders that want to convert more visitors into customers with a high performance website, exceptional product design, and a memorable brand, but lack the necessary time, skill, and/or expertise to do it all on their own.',
+  },
+  {
+    question: "Who's steering this ship anyway?",
+    answer:
+      "I've been designing products and improving user experience for agencies, startups, and small businesses for over 10 years. I've built and sold a few SaaS products of my own, and mentored 200+ designers through intensive 3-6 month UX bootcamps and other short courses.",
+  },
+];
